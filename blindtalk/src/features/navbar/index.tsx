7aac84a115afed0de1910c1b0532f2d9aeac2944
wisholli <osribna4@gmail.com>
2023-02-28 19:33:49 +0200

    navbar extension

--- blindtalk/src/features/navbar/index.tsx
+++ blindtalk/src/features/navbar/index.tsx
@@ -1,7 +1,17 @@
+import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
   return (
-    <div className="flex items-center justify-between py-5 bg-basic-gray-5 mb-5 text-3xl">
+    <div className="flex justify-between w-full py-5 bg-basic-gray-5 mb-5 text-3xl">
       <h1 className="ml-4">BlindTalk</h1>
+      <div className="text-xl">
+        <NavLink to={"/"} className="mr-3">
+          Find new friend
+        </NavLink>
+        <NavLink to={"/conversation"} className="mr-3">
+          Conversations
+        </NavLink>
+      </div>
     </div>
   );
 };
