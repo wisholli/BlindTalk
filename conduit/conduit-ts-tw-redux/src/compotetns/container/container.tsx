0ce2d1eb1fb5fcee136269358ba3f6c98ac5f426
wisholli <osribna4@gmail.com>
2023-05-08 14:00:35 +0300

    file system change

--- conduit/conduit-ts-tw-redux/src/compotetns/container/container.tsx
+++
@@ -1,8 +0,0 @@
-import { FC, PropsWithChildren } from "react";
-interface ContainerProps {}
-
-export const Container: FC<PropsWithChildren<ContainerProps>> = ({
-  children,
-}) => {
-  return <div className="max-w-screen-xl mx-auto">{children}</div>;
-};
--- conduit/conduit-ts-tw-redux/src/compotetns/mosules/feed/components/tag-list/tag-list.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-
-interface TagListProps {}
-
-export const TagList: FC<TagListProps> = ({}) => {
-  return <div>Popular tags</div>;
-};
