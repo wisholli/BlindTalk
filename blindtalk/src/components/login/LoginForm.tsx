37a034f47015f37c9cec7c3da810fc6231967b11
wisholli <osribna4@gmail.com>
2023-05-11 16:37:25 +0300

    added style for login page

--- blindtalk/src/components/login/LoginForm.tsx
+++ blindtalk/src/components/login/LoginForm.tsx
@@ -1,5 +1,6 @@
 import { Formik, Form, Field } from "formik";
 import { AuthData } from "../../types";
+import { NavLink } from "react-router-dom";
 
 type Props = {
   onLogin: ({ email, password }: AuthData) => void;
@@ -13,42 +14,48 @@ interface MyFormValues {
 export const LoginForm = ({ onLogin }: Props) => {
   const initialValues: MyFormValues = { email: "", password: "" };
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className=" mt-24 mx-4 md:mx-36 lg:mt-32 lg:mx-96">
       <Formik
         initialValues={initialValues}
         onSubmit={({ email, password }) => {
           onLogin({ email, password });
         }}
       >
-        <Form className="flex flex-col items-center justify-center mb-12 box-border h-50 w-50 p-4 border-2 rounded-md bg-basic-gray-5">
-          <h1 className="text-center mb-2">Welcome back!</h1>
-          <div className="mb-2">
-            <div className="mb-1">
-              <label htmlFor="email" className="mr-1">
-                Email
-              </label>
-              <Field
-                id="email"
-                name="email"
-                placeholder="Email"
-                className="bg-gray-5"
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="mr-1">
-                Password
-              </label>
-              <Field
-                id="password"
-                name="password"
-                placeholder="Password"
-                className="bg-gray-5"
-              />
-            </div>
+        <Form>
+          <h1 className="font-pacifico font-normal text-5xl mb-10 text-black-200 text-center lg:mb-14 lg:text-6xl">
+            Welcome back!
+          </h1>
+          <Field
+            id="email"
+            name="email"
+            placeholder="Email"
+            className="w-full font-maven font-normal text-2xl text-gray-100 border-b border-black-200 mb-8 placeholder:font-maven placeholder:font-normal placeholder:text-2xl placeholder:text-gray-100 focus:outline-none md:placeholder:text-3xl md:text-3xl lg:mb-16 "
+          />
+          <Field
+            id="password"
+            name="password"
+            placeholder="Password"
+            className="w-full font-maven font-normal text-2xl text-gray-100 border-b border-black-200 mb-8 placeholder:font-maven placeholder:font-normal placeholder:text-2xl placeholder:text-gray-100 focus:outline-none md:placeholder:text-3xl md:text-3xl lg:mb-16"
+          />
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className=" border rounded-[48px] bg-green-100 font-maven w-full text-2xl  text-white font-medium py-3 px-28  mb-2 lg:w-auto lg:text-4xl "
+            >
+              Sing in
+            </button>
           </div>
 
-          <div className="text-center text-blue-500">
-            <button type="submit">Sing in</button>
+          <div className="flex justify-center items-center">
+            <p className="mr-2 font-maven font-normal text-black-100 text-xl md:text-2xl">
+              Don’t have an account?
+            </p>
+            <NavLink
+              to={"/register"}
+              className="font-maven font-normal text-black-100 text-xl md:text-2xl underline underline-offset-4"
+            >
+              Sing up
+            </NavLink>
           </div>
         </Form>
       </Formik>
