7aa5be6259be17f2826e56701c492a4640609d2b
wisholli <osribna4@gmail.com>
2023-10-04 15:05:58 +0100

    edited login form

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -1,4 +1,5 @@
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, useFormik } from "formik";
+import * as Yup from "yup";
 import { AuthData } from "../../types";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
@@ -18,64 +19,71 @@ export const LoginForm = ({ onLogin }: Props) => {
   //show and hide password
   let [isPassword, setIsPassword] = useState<boolean>(false);
 
-  const initialValues: MyFormValues = { email: "", password: "" };
+  //formik
+  const formik = useFormik<MyFormValues>({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (loginData: MyFormValues) => {
+      onLogin(loginData);
+    },
+  });
+
   return (
     <div className=" mt-24 mx-4 md:mx-36 lg:mt-32 lg:mx-96">
-      <Formik
-        initialValues={initialValues}
-        onSubmit={({ email, password }) => {
-          onLogin({ email, password });
-        }}
-      >
-        <Form>
-          <h1 className="font-pacifico font-normal text-5xl mb-10 text-black-200 text-center lg:mb-14 lg:text-6xl">
-            Welcome back!
-          </h1>
-          <Field
-            id="email"
-            name="email"
-            placeholder="Email"
-            className="w-full font-maven font-normal text-2xl text-black-200 border-b border-black-200 mb-8 placeholder:font-maven placeholder:font-normal placeholder:text-2xl placeholder:text-gray-100 focus:outline-none md:placeholder:text-3xl md:text-3xl lg:mb-16 "
+      <form onSubmit={formik.handleSubmit}>
+        <h1 className="font-pacifico font-normal text-5xl mb-10 text-black-200 text-center lg:mb-14 lg:text-6xl">
+          Welcome back!
+        </h1>
+        <input
+          id="email"
+          name="email"
+          placeholder="Email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          className="w-full font-maven font-normal text-2xl text-black-200 border-b border-black-200 mb-8 placeholder:font-maven placeholder:font-normal placeholder:text-2xl placeholder:text-gray-100 focus:outline-none md:placeholder:text-3xl md:text-3xl lg:mb-16 "
+        />
+        <div className="flex justify-between items-center border-b border-black-200 mb-8 lg:mb-16">
+          <input
+            id="password"
+            name="password"
+            type={isPassword ? "text" : "password"}
+            placeholder="Password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+            className="w-full font-maven font-normal text-2xl text-black-200   placeholder:font-maven placeholder:font-normal placeholder:text-2xl placeholder:text-gray-100 focus:outline-none md:placeholder:text-3xl md:text-3xl "
           />
-          <div className="flex justify-between items-center border-b border-black-200 mb-8 lg:mb-16">
-            <Field
-              id="password"
-              name="password"
-              type={isPassword ? "text" : "password"}
-              placeholder="Password"
-              className="w-full font-maven font-normal text-2xl text-black-200   placeholder:font-maven placeholder:font-normal placeholder:text-2xl placeholder:text-gray-100 focus:outline-none md:placeholder:text-3xl md:text-3xl "
+          <button type="button" onClick={() => setIsPassword(!isPassword)}>
+            <img
+              src={isPassword ? eyeOffIcon : eyeIcon}
+              alt="eye-icon"
+              className="h-10 w-10"
             />
-            <button type="button" onClick={() => setIsPassword(!isPassword)}>
-              <img
-                src={isPassword ? eyeOffIcon : eyeIcon}
-                alt="eye-icon"
-                className="h-10 w-10"
-              />
-            </button>
-          </div>
+          </button>
+        </div>
 
-          <div className="flex justify-center">
-            <button
-              type="submit"
-              className=" border rounded-[48px] bg-green-100 font-maven w-full text-2xl  text-white font-medium py-3 px-28  mb-2 lg:w-auto lg:text-4xl "
-            >
-              Sing in
-            </button>
-          </div>
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className=" border rounded-[48px] bg-green-100 font-maven w-full text-2xl  text-white font-medium py-3 px-28  mb-2 lg:w-auto lg:text-4xl "
+          >
+            Sing in
+          </button>
+        </div>
 
-          <div className="flex justify-center items-center">
-            <p className="mr-2 font-maven font-normal text-black-100 text-xl md:text-2xl">
-              Don’t have an account?
-            </p>
-            <NavLink
-              to={"/register"}
-              className="font-maven font-normal text-black-100 text-xl md:text-2xl underline underline-offset-4"
-            >
-              Sing up
-            </NavLink>
-          </div>
-        </Form>
-      </Formik>
+        <div className="flex justify-center items-center">
+          <p className="mr-2 font-maven font-normal text-black-100 text-xl md:text-2xl">
+            Don’t have an account?
+          </p>
+          <NavLink
+            to={"/register"}
+            className="font-maven font-normal text-black-100 text-xl md:text-2xl underline underline-offset-4"
+          >
+            Sing up
+          </NavLink>
+        </div>
+      </form>
     </div>
   );
 };
